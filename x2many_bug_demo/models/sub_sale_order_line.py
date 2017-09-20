012855ae310f6530526692a5904df6fff0363e18
Graeme Gellatly <gdgellatly@gmail.com>
2017-09-21 11:50:18 +1200

    Test 6: Give me print output on save - product_id added /home/graemeg/PycharmProjects/odoo10-c/python-odoo11/bin/python /home/graemeg/PycharmProjects/odoo10-c/odoo/odoo-bin -c ../etc/.odoo11_ent_testing -u x2many_bug_demo -d odoo11-test2 --log-level=debug_rpc --workers=0 Order Create {'user_id': 1, 'campaign_id': False, 'order_line': [[0, 'virtual_97', {'product_qty': 1, 'product_packaging': False, 'discount': 0, 'product_uom': 1, 'price_unit': 1, 'product_uom_qty': 1, 'route_id': False, 'name': '[C.40940G550ZR8ISA] Coil 0.40 940 G550 Zinacore (Ironsand)', 'tax_id': [[6, False, []]], 'sub_order_line_ids': [[0, 'virtual_128', {}], [0, 'virtual_131', {}]], 'analytic_tag_ids': [[6, False, []]], 'customer_lead': 0, 'layout_category_id': False, 'sequence': 0, 'product_id': 3}]], 'delivery_rating_success': False, 'requested_date': False, 'carrier_id': False, 'fiscal_position_id': False, 'opportunity_id': False, 'pricelist_id': 1, 'picking_policy': 'one', 'origin': False, 'source_id': False, 'payment_term_id': False, 'partner_shipping_id': 7, 'note': '', 'team_id': 1, 'partner_invoice_id': 7, 'date_order': '2017-09-20 23:46:46', 'incoterm': False, 'medium_id': False, 'analytic_account_id': False, 'client_order_ref': False, 'partner_id': 7, 'company_id': 3, 'validity_date': False, 'warehouse_id': 2} Order Line Create {'order_id': 8, 'product_qty': 1, 'product_packaging': False, 'discount': 0, 'product_uom': 1, 'price_unit': 1, 'product_uom_qty': 1, 'route_id': False, 'name': '[C.40940G550ZR8ISA] Coil 0.40 940 G550 Zinacore (Ironsand)', 'tax_id': [[6, False, []]], 'sub_order_line_ids': [[0, 'virtual_128', {}], [0, 'virtual_131', {}]], 'analytic_tag_ids': [[6, False, []]], 'customer_lead': 0, 'layout_category_id': False, 'sequence': 0, 'product_id': 3} Sub Line Create {'sale_line_id': 11} Sub Line Create {'sale_line_id': 11} Order Write {'message_follower_ids': []}

--- x2many_bug_demo/models/sub_sale_order_line.py
+++ x2many_bug_demo/models/sub_sale_order_line.py
@@ -13,6 +13,19 @@ class SubSaleOrderLine(models.Model):
     name = fields.Char()
     product_id = fields.Many2one(comodel_name='product.product')
     sale_line_id = fields.Many2one(comodel_name='sale.order.line')
+    non_conflicting_field_name = fields.Char()
+
+    @api.model
+    def create(self, vals):
+        print('Sub Line Create')
+        print(vals)
+        return super(SubSaleOrderLine, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        print('Sub Line Write')
+        print(vals)
+        return super(SubSaleOrderLine, self).write(vals)
 
 
 class SaleOrderLine(models.Model):
@@ -23,3 +36,31 @@ class SaleOrderLine(models.Model):
         comodel_name='sub.sale.order.line',
         inverse_name='sale_line_id',
     )
+
+    @api.multi
+    def write(self, vals):
+        print('Order Line Write')
+        print(vals)
+        return super(SaleOrderLine, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        print('Order Line Create')
+        print(vals)
+        return super(SaleOrderLine, self).create(vals)
+
+
+class Sale(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def write(self, vals):
+        print('Order Write')
+        print(vals)
+        return super(Sale, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        print('Order Create')
+        print(vals)
+        return super(Sale, self).create(vals)
